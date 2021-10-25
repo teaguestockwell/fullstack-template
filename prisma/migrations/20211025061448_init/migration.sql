2987a76492a35570767ac7e95e754cfc466b6c87
teagueStockwell <tsAppDevelopment@gmail.com>
2021-10-24 23:16:24 -0700

    Add preview flags in prisma for planetscale

--- prisma/migrations/20211025061448_init/migration.sql
+++ prisma/migrations/20211025061448_init/migration.sql
@@ -1,4 +1,19 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `oauthName` VARCHAR(191) NOT NULL,
+    `oauthImgSrc` VARCHAR(191) NOT NULL,
+    `about` VARCHAR(191) NOT NULL DEFAULT '',
+    `lastSignIn` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `GameSession` (
     `id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -19,9 +34,3 @@ CREATE TABLE `Feedback` (
     UNIQUE INDEX `Feedback_id_key`(`id`),
     PRIMARY KEY (`userId`, `gameSessionId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Feedback` ADD CONSTRAINT `Feedback_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Feedback` ADD CONSTRAINT `Feedback_gameSessionId_fkey` FOREIGN KEY (`gameSessionId`) REFERENCES `GameSession`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
